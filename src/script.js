e60a057dc6f0f30531fc6400a2d6225f1c5451ba
Ghayas <ghayasleo99@gmail.com>
2022-09-01 10:00:22 +0500

    ⭐⭐⭐ Course: Transform Objects

--- src/script.js
+++ src/script.js
@@ -13,11 +13,21 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(1, -0.6, -1)
+mesh.scale.set(2, 0.5, 0.5)
+mesh.rotation.reorder("YXZ")
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
 scene.add(mesh)
+
+// axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 // camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 2
+camera.position.set(1, 1, 3)
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // renderer
