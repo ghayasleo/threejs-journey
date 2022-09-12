9c9573b7a2513b662eaf8eec3a5a730ac719b1fd
Ghayas <ghayasleo99@gmail.com>
2022-09-12 15:06:46 +0500

    changes made

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import * as dat from 'lil-gui'
 
 /**
@@ -18,60 +16,13 @@ const canvas = document.querySelector('#webgl')
 const scene = new THREE.Scene()
 
 /**
- * Models
+ * Test sphere
  */
-
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath("/draco/")
-
-const gltfLoader = new GLTFLoader()
-gltfLoader.setDRACOLoader(dracoLoader)
-
-let mixer = null
-
-gltfLoader.load('/models/Fox/glTF/Fox.gltf', (gltf) => {
-  mixer = new THREE.AnimationMixer(gltf.scene)
-  const action = mixer.clipAction(gltf.animations[2])
-  action.play()
-
-  gltf.scene.scale.set(0.03, 0.03, 0.03)
-  scene.add(gltf.scene)
-
-  // while (gltf.scene.children.length) scene.add(gltf.scene.children[0])
-})
-
-
-/**
- * Floor
- */
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshStandardMaterial({
-    color: '#444444',
-    metalness: 0,
-    roughness: 0.5
-  })
+const testSphere = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 32, 32),
+  new THREE.MeshStandardMaterial()
 )
-floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
-scene.add(floor)
-
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
-scene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
+scene.add(testSphere)
 
 /**
  * Sizes
@@ -96,45 +47,49 @@ window.addEventListener('resize', () => {
 })
 
 /**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(0.25, 3, - 2.25)
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1, 0xffffff)
+scene.add(directionalLight, directionalLightHelper)
+
+/**
  * Camera
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(2, 2, 2)
+camera.position.set(4, 1, - 4)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
 controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  antialias: true,
+  physicallyCorrectLights: true,
+  outputEncoding: THREE.sRGBEncoding,
+  toneMapping: THREE.CineonToneMapping,
+  toneMappingExposure: 1.75,
+  shadowMap: {
+    enabled: true,
+    type: THREE.PCFSoftShadowMap
+  },
 })
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-let previousTime = 0
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
-  const deltaTime = elapsedTime - previousTime
-  previousTime = elapsedTime
-
-
-  if (mixer) mixer.update(deltaTime)
-
   // Update controls
-  controls.update(deltaTime)
+  controls.update()
 
   // Render
   renderer.render(scene, camera)
